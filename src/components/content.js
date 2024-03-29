4556444df477559a983ffb93d4a347664d1a7ae9
yash2316 <guadeyash9@gmail.com>
2024-03-30 00:23:48 +0530

    2nd commit

--- src/components/content.js
+++ src/components/content.js
@@ -1,34 +1,34 @@
 import { useEffect, useState } from "react";
 import Card from "../components/news-card";
 
-export default function Content({newsList, setNewsList, topic}){
+export default function Content({ newsList, setNewsList, topic }) {
 
 
-const handleFetch = async ()=>{
-    console.log("flag -1", topic);
+    const handleFetch = async () => {
 
-    if(newsList.length!=0)
-        return;
+        if (newsList.length != 0)
+            return;
 
-    console.log("flag 0", topic);
+        const response = await fetch("https://fastapi-app-h5pz.vercel.app/api/" + topic);
+        const data = await response.json();
+        console.log(data.news[0]);
+        //setContent(data.news);
+        setNewsList(data.news);
+    }
 
-    const response = await fetch("https://fastapi-app-h5pz.vercel.app/api/" + topic);
-    const data = await response.json();
-    //console.log(data.news[0]);
-    //setContent(data.news);
-    setNewsList(data.news);
-}
-
-useEffect(()=>{
-    handleFetch();
-}, [topic]);
+    useEffect(() => {
+        handleFetch();
+    }, [topic]);
 
 
     return (
-        <div> 
-            {newsList.length==0 ?  (<p>Loading .....</p>) : null }
-            {newsList && newsList.map((c)=> <Card title={c.title} imgUrl={c.photo_url} siteUrl={c.link} />)}
+        <div>
+
+            {newsList.length == 0 ? (<p>Loading .....</p>) : null}
+            {newsList && newsList.map((c) => <Card obj={c} key={newsList.indexOf(c)} />)}
         </div>
     );
 
 }
+
+
--- src/components/news-card.js
+++ src/components/news-card.js
@@ -1,44 +1,50 @@
 import axios from 'axios';
-import cheerio from 'cheerio';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
-export default function Card({ title, imgUrl, siteUrl }) {
+export default function Card({obj}) {
+
+    const title = obj.title;
+    const sourceUrl = obj.url;
+    const imgUrl = obj.urlToImage;
+    const date = obj.publishedAt;
+    const source = obj.source.name;
+    const author = obj.author;
 
     const corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com/';
 
-    const [summary, setSummary] = useState("");
-
+    const [summary, setSummary] = useState([]);
+   
     async function handleSumm(u) {
 
-        const url = 'https://news-article-data-extract-and-summarization1.p.rapidapi.com/extract/';
         const options = {
             method: 'POST',
+            url: 'https://news-article-data-extract-and-summarization1.p.rapidapi.com/extract/',
             headers: {
-                'content-type': 'application/json',
-                'X-RapidAPI-Key': '7bbb7cd865mshd68e00cace314fep11d10ejsnd6d281102968',
-                'X-RapidAPI-Host': 'news-article-data-extract-and-summarization1.p.rapidapi.com'
+              'content-type': 'application/json',
+              'X-RapidAPI-Key': '7bbb7cd865mshd68e00cace314fep11d10ejsnd6d281102968',
+              'X-RapidAPI-Host': 'news-article-data-extract-and-summarization1.p.rapidapi.com'
             },
-            body: {
-                url: u + ""
+            data: {
+              url: u
             }
-        };
-
-        try {
-            const response = await fetch(url, options);
-            const result = await response.text();
-            console.log(result);
-        } catch (error) {
-            console.error(error);
-        }
+          };
+          
+          try {
+              const response = await axios.request(options);
+              const result = await response.data.summary;
+              setSummary(result);
+              
+          } catch (error) {
+              console.error(error);
+          }
 
     }
 
 
     const url = "https://cdn.britannica.com/69/228369-050-0B18A1F6/Asian-Cup-Final-2019-Hasan-Al-Haydos-Qatar-Japan-Takumi-Minamino.jpg";
 
-    const img = {
-        backgroundImage: `url('${imgUrl}')`,
+    const css = {
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
@@ -49,15 +55,31 @@ export default function Card({ title, imgUrl, siteUrl }) {
 
     return (
 
-        <div style={img}>
-
-            <a href={siteUrl}><h1>{title}</h1></a>
-            <button onClick={() => { handleSumm(siteUrl) }}>Summarize</button>
+        <div style={css}>
+            
+            <p>Author : {author}</p>
+            <p>Source: {source}</p>
+            <a href={sourceUrl}><h1>{title}</h1></a>
+            <img src={imgUrl} style={{width:"450px"}}/>
+            <button onClick={() => { handleSumm(sourceUrl) }}>Summarize</button>
             <p>{summary}</p>
-
+            <p>Published at : {date}</p>
         </div>
 
     );
 }
 
 
+// {
+//     "publishedAt": "2024-03-28T17:03:00Z",
+//         "author": "Pathikrit Chakraborty",
+//             "urlToImage": "https://static.toiimg.com/thumb/msid-108858783,width-1070,height-580,imgsize-74418,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg",
+//                 "description": "India News: LUCKNOW: Jailed mafia gangster Mukhtar Ansari, 63, died due to cardiac attack during treatment at Banda medical college on Thursday night.",
+//                     "source": {
+//         "name": "The Times of India",
+//             "id": "the-times-of-india"
+//     },
+//     "title": "Jailed gangster Mukhtar Ansari passes away due to cardiac arrest | India News - The Times of India",
+//         "url": "https://timesofindia.indiatimes.com/india/jailed-gangster-mukhtar-ansari-passes-away/articleshow/108858786.cms",
+//             "content": null
+// }
